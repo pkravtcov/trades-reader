5fc1326f33e58f7c22d3bf214249b440a56b177f
irinakravtcova <irinakravtcova@irinas-mbp-2.lan>
2020-04-25 09:04:10 -0400

    TASK-2 Adding hashCode and equals to object, fixing firms input file

--- src/main/java/com/lste/trades/data/StartTimeCounter.java
+++ src/main/java/com/lste/trades/data/StartTimeCounter.java
@@ -20,5 +20,33 @@ public class StartTimeCounter {
 	public void setOrderCount(int orderCount) {
 		this.orderCount = orderCount;
 	}
-
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + orderCount;
+		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof StartTimeCounter)) {
+			return false;
+		}
+		StartTimeCounter other = (StartTimeCounter) obj;
+		if (orderCount != other.orderCount) {
+			return false;
+		}
+		if (startTime == null) {
+			if (other.startTime != null) {
+				return false;
+			}
+		} else if (!startTime.equals(other.startTime)) {
+			return false;
+		}
+		return true;
+	}
 }
